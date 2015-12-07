4ca2eefd435290bb0fe383c25280e00e0eb7c7f2
PhoenixAvenger <thomas.r.nagl@gmail.com>
2015-12-06 19:28:31 -0500

    Fixed css injection and flair size.

--- fadetheflairs.js
+++ fadetheflairs.js
@@ -1,12 +1,7 @@
-var cssId = 'fadeTheFlairs';  // you could encode the css path itself to generate id..
-if (!document.getElementById(cssId))
-{
-    var head  = document.getElementsByTagName('head')[0];
-    var link  = document.createElement('link');
-    link.id   = cssId;
-    link.rel  = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'https://raw.githubusercontent.com/PhoenixAvenger/r-nfl-fade-the-flairs/master/fadetheflairs.css';
-    link.media = 'all';
-    head.appendChild(link);
-}
+link = document.createElement( "link" );
+link.href = "https://raw.githubusercontent.com/PhoenixAvenger/r-nfl-fade-the-flairs/master/fadetheflairs.css"
+link.type = "text/css";
+link.rel = "stylesheet";
+link.media = "screen,print";
+
+document.getElementsByTagName( "head" )[0].appendChild( link );
